57c29fa09dae2bf921dcdc9f59460e3f269bf875
Igor Couto <igor.fcouto@gmail.com>
2023-12-23 20:53:32 +0000

    Create web api project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using DesignPatterns.Facade;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+
+app.Run();
